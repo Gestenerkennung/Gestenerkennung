18f3a1d217d64ecb9608c51cc1a470baa2732a7c
Gestenerkennung <35739516+Gestenerkennung@users.noreply.github.com>
2018-02-05 02:30:47 +0100

    Update Form1.cs

--- PCD_Combinder/PCD_Combinder/Form1.cs
+++ PCD_Combinder/PCD_Combinder/Form1.cs
@@ -31,7 +31,7 @@ namespace PCD_Combinder
                 text += File.ReadAllText(f.FullName);
                 File.Delete(f.FullName);
             }
-            return text;
+            return text.Replace(",g", "").Replace(",", " ");
         }
         private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -40,13 +40,24 @@ namespace PCD_Combinder
             {
                 String path = folderBrowserDialog1.SelectedPath;
                 String AllText = "";
-                int i = 0, j;
+                /*
+                 * k = Gestenklasse (0 - 3)
+                 * g = Beispiel (also in diesem Fall von 0 - 99, da jeweils 100 Testgeesten)
+                 */
+                int i = 0, j, k, tmp;
+                int[] a = new int[4];
                 for(; i<10; i++)
                 {
                     for (j = 0; j < 40; j++)
                     {
                         AllText = getText(path + @"/p" + i + @"/g" + j + @"/cropped");
-                        File.WriteAllText(path + @"/p" + i + @"/g" + j + @"/cropped" + @"/konvertiert.txt", AllText.Replace(",g", "").Replace(",", " "));
+                        File.WriteAllText(path+@"/lcc_p1_k" + k + "_g" + a[k] + ".txt", AllText.Substring(0,AllText.Length-2));
+                        a[k]++;
+                        if (++tmp == 10) //Limit 10, da eine Person jeweils 10x eine Geste aufgenommen hat.
+                        {
+                            k++;
+                            tmp = 0;
+                        }
                     }
                 }
                 MessageBox.Show(this, "Erfolgreich konvertiert!", "Beendet", MessageBoxButtons.OK, MessageBoxIcon.Information);
